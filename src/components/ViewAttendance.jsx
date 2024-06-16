d484faa07048ec65e6b8e82688ba5a1fb06ec65f
MuhammadMoon <muhammadmoon033@gmail.com>
2024-06-16 20:51:17 +0500

    add-print-att

--- src/components/ViewAttendance.jsx
+++ src/components/ViewAttendance.jsx
@@ -1,22 +1,25 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { format, parseISO, addDays, startOfMonth, endOfMonth } from "date-fns";
+'use client'
+import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
+import { format, parseISO, addDays, startOfMonth, endOfMonth } from 'date-fns';
 import { FiEye } from 'react-icons/fi';
+import { useReactToPrint } from 'react-to-print';
 import { enUS } from 'date-fns/locale';
 
 const ViewAttendance = () => {
+  const componentRef = useRef();
   const [loading, setLoading] = useState(false);
   const [teachers, setTeachers] = useState([]);
-  const [selectedTeacher, setSelectedTeacher] = useState("");
+  const [selectedTeacher, setSelectedTeacher] = useState('');
   const [classes, setClasses] = useState([]);
   const [sections, setSections] = useState([]);
   const [subjects, setSubjects] = useState([]);
   const [attendanceData, setAttendanceData] = useState([]);
   const [notFound, setNotFound] = useState(false);
   const [students, setStudents] = useState([]);
-  const [selectedClass, setSelectedClass] = useState("");
-  const [selectedSection, setSelectedSection] = useState("");
-  const [selectedSubject, setSelectedSubject] = useState("");
+  const [selectedClass, setSelectedClass] = useState('');
+  const [selectedSection, setSelectedSection] = useState('');
+  const [selectedSubject, setSelectedSubject] = useState('');
   const [selectedMonth, setSelectedMonth] = useState(
     new Date().toISOString().slice(0, 7)
   );
@@ -25,10 +28,10 @@ const ViewAttendance = () => {
     const fetchTeachers = async () => {
       setLoading(true);
       try {
-        const response = await axios.get("/api/admin/all-teachers");
+        const response = await axios.get('/api/admin/all-teachers');
         setTeachers(response.data.teachers);
       } catch (error) {
-        console.error("Error fetching teachers", error);
+        console.error('Error fetching teachers', error);
       }
       setLoading(false);
     };
@@ -36,10 +39,10 @@ const ViewAttendance = () => {
     const fetchStudents = async () => {
       setLoading(true);
       try {
-        const response = await axios.get("/api/admin/all-students");
+        const response = await axios.get('/api/admin/all-students');
         setStudents(response.data.students);
       } catch (error) {
-        console.error("Error fetching students", error);
+        console.error('Error fetching students', error);
       }
       setLoading(false);
     };
@@ -63,7 +66,7 @@ const ViewAttendance = () => {
       setAttendanceData([]);
       setNotFound(false);
     } catch (error) {
-      console.error("Error fetching teacher details", error);
+      console.error('Error fetching teacher details', error);
     }
     setLoading(false);
   };
@@ -85,7 +88,7 @@ const ViewAttendance = () => {
       } else {
         // Filter attendance data to only include entries from the selected month
         const filteredAttendance = attendance.filter((att) => {
-          const attMonth = format(parseISO(att.date), "yyyy-MM");
+          const attMonth = format(parseISO(att.date), 'yyyy-MM');
           return attMonth === selectedMonth;
         });
 
@@ -93,7 +96,7 @@ const ViewAttendance = () => {
         setNotFound(false);
       }
     } catch (error) {
-      console.error("Error fetching attendance", error);
+      console.error('Error fetching attendance', error);
       setNotFound(true);
       setAttendanceData([]);
     }
@@ -101,155 +104,176 @@ const ViewAttendance = () => {
   };
 
   const renderDays = () => {
-  const dayNames = [];
-  const dates = [];
-  const dateFormat = 'EEE'; // Format for displaying day names (e.g., Mon, Tue, etc.)
+    const dayNames = [];
+    const dates = [];
+    const dateFormat = 'EEE'; // Format for displaying day names (e.g., Mon, Tue, etc.)
 
-  // Get the first day of the selected month
-  const firstDayOfMonth = startOfMonth(new Date(selectedMonth));
-  const lastDayOfMonth = endOfMonth(new Date(selectedMonth));
-  const numberOfDays = lastDayOfMonth.getDate();
+    // Get the first day of the selected month
+    const firstDayOfMonth = startOfMonth(new Date(selectedMonth));
+    const lastDayOfMonth = endOfMonth(new Date(selectedMonth));
+    const numberOfDays = lastDayOfMonth.getDate();
 
-  // Render the day names
-  for (let i = 1; i <= numberOfDays; i++) {
-    const dayName = format(addDays(firstDayOfMonth, i - 1), dateFormat, { locale: enUS });
-    dayNames.push(
-      <th key={`dayName-${i}`} className="py-2 px-4 border text-xs">
-        {dayName}
-      </th>
+    // Render the day names
+    for (let i = 1; i <= numberOfDays; i++) {
+      const dayName = format(addDays(firstDayOfMonth, i - 1), dateFormat, {
+        locale: enUS,
+      });
+      dayNames.push(
+        <th key={`dayName-${i}`} className="text-[8px] py-2 px-2 border ">
+          {dayName}
+        </th>
+      );
+      dates.push(
+        <th key={`date-${i}`} className="text-[8px] py-2 px-2 border ">
+          {i}
+        </th>
+      );
+    }
+
+    return (
+      <>
+        <tr className=''> 
+          <th className="text-[8px] py-2 px-4 border" >SR.NO</th>
+          <th className="text-[8px] py-2 px-4 border">Name</th>
+          <th className="text-[8px] py-2 px-4 border">RN</th>
+          {dayNames}
+          <th className="text-[8px] py-2 px-4 border"></th>
+          <th className="text-[8px] py-2 px-4 border bg-blue-200">Total</th>
+        </tr>
+        <tr>
+          <th className="text-[8px] py-2 px-4 border"></th>
+          <th className="text-[8px] py-2 px-4 border"></th>
+          <th className="text-[8px] py-2 px-4 border"></th>
+          {dates}
+          <th className="text-[8px] py-2 px-4 border"></th>
+          <th className="text-[8px] py-2 px-4 border"></th>
+        </tr>
+      </>
     );
-    dates.push(
-      <th key={`date-${i}`} className="py-2 px-4 border text-xs">
-        {i}
-      </th>
-    );
-  }
-
-  return (
-    <>
-      <tr>
-        <th className="text-xs py-2 px-4 border">SR.NO</th>
-        <th className="text-xs py-2 px-4 border">Name</th>
-        <th className="text-xs py-2 px-4 border">RN</th>
-        {dayNames}
-        <th className="text-xs py-2 px-4 border"></th>
-        <th className="text-xs py-2 px-4 border  bg-green-300 ">Total</th>
-      </tr>
-      <tr>
-        <th className="text-xs py-2 px-4 border"></th>
-        <th className="text-xs py-2 px-4 border"></th>
-        <th className="text-xs py-2 px-4 border"></th>
-        {dates}
-        <th className="text-xs py-2 px-4 border"></th>
-        <th className="text-xs py-2 px-4 border"></th>
-      </tr>
-    </>
-  );
-};
+  };
 
   const renderAttendanceRows = () => {
     if (attendanceData.length === 0) return null;
-  
+
     const studentAttendanceMap = new Map();
     const monthTotal = Array(31).fill(0); // Array to store monthly totals
-  
+
     attendanceData.forEach((attdata) => {
       attdata.students.forEach((studentData) => {
         if (!studentAttendanceMap.has(studentData.rollNumber)) {
           studentAttendanceMap.set(studentData.rollNumber, {
             id: studentData.id,
             rollNumber: studentData.rollNumber,
-            attendance: Array(31).fill("-"),
+            attendance: Array(31).fill('-'),
             totalPresent: 0,
           });
         }
-  
+
         const attendanceArray = studentAttendanceMap.get(
           studentData.rollNumber
         ).attendance;
         const day = new Date(attdata.date).getDate() - 1;
-        attendanceArray[day] = studentData.isPresent ? "P" : "A";
-  
+        attendanceArray[day] = studentData.isPresent ? 'P' : 'A';
+
         if (studentData.isPresent) {
           studentAttendanceMap.get(studentData.rollNumber).totalPresent++;
           monthTotal[day]++; // Increment the day's total present count
         }
       });
     });
-  
+
     const rows = [];
     let index = 1;
     studentAttendanceMap.forEach((studentData) => {
       const student = students.find((s) => s._id === studentData.id);
-      const studentName = student ? student.Name : "Unknown";
-  
+      const studentName = student ? student.Name : 'Unknown';
+
       const attendanceCells = studentData.attendance.map((att, dayIndex) => (
         <td
-          key={`${studentData.rollNumber}-${dayIndex}`}
-          className={`text-xs py-2 px-4 border text-center ${
-            att === "A" ? "bg-red-200" : ""
-          }`}
-        >
-          {att}
-        </td>
+  key={`${studentData.rollNumber}-${dayIndex}`}
+  className={`text-[8px] border text-center ${
+    att === 'A' ? 'bg-red-200' : att === 'P' ? 'bg-green-200' : ''
+  }`}
+>
+  {att}
+</td>
+
       ));
-  
+
       rows.push(
         <tr key={studentData.rollNumber}>
-          <td className="text-xs py-2 px-4 border text-center">{index}</td>
-          <td className="text-xs py-2 px-4 border text-center">{studentName}</td>
-          <td className="text-xs py-2 px-4 border text-center">{studentData.rollNumber}</td>
+          <td className="text-[8px]  border text-center">{index}</td>
+          <td className="text-[8px]  border text-center">{studentName}</td>
+          <td className="text-[8px]  border text-center">
+            {studentData.rollNumber}
+          </td>
           {attendanceCells}
-          <td className="text-xs  py-2 px-4 border text-center">{studentData.totalPresent}</td>
+          <td className="text-[8px]    border text-center bg-blue-200">
+            {studentData.totalPresent}
+          </td>
         </tr>
       );
-  
+
       index++;
     });
-  
+
     // Render the row for the total present count for each day of the month
     const totalRow = (
       <tr key="totalRow">
-        <td className="text-xs py-2 px-4 border text-center"></td>
-        <td className="text-xs py-2 px-4 border text-center font-semibold  bg-green-300">Total</td>
-        <td className="text-xs py-2 px-4 border text-center"></td>
+        <td className="text-[8px] border text-center"></td>
+        <td className="text-[8px] border text-center font-semibold bg-blue-200">
+          Total
+        </td>
+        <td className="text-[8px] border text-center"></td>
         {monthTotal.map((total, index) => (
           <td
             key={`total-${index}`}
-            className={`text-xs py-2 px-4 border text-center ${
-              total > 0 ? "bg-green-100" : ""
+            className={`text-[8px] border text-center ${
+              total > 0 ? 'bg-blue-200' : ''
             }`}
           >
-            {total > 0 ? total : "-"}
+            {total > 0 ? total : '-'}
           </td>
         ))}
-        <td className="text-sm py-2 px-4 border text-center bg-green-300 font-semibold  ">{monthTotal.reduce((acc, curr) => acc + curr, 0)}</td>
+        <td className="text-[8px] border text-center bg-purple-400 font-semibold  ">
+          {monthTotal.reduce((acc, curr) => acc + curr, 0)}
+        </td>
       </tr>
     );
-  
+
     rows.push(totalRow);
-  
+
     return rows;
   };
-      
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    pageStyle: `
+      @page {
+        size: A4;
+        margin: 10px;
+      }
+    `,
+  });
+  
 
   return (
-    <div className="min-h-screen mt-28 px-10">
-      <div className="container mx-auto p-4">
-        <h1 className="text-sm flex justify-start items-start text-blue-500 mb-10 text-center">
+    <div className=" w-full  px-4 " id="component-to-print" >
+      <div className="container  " ref={componentRef}>
+        <h1 className="text-xsm flex justify-start items-start text-blue-500 mb-10 text-center">
           View Attendance
         </h1>
-        <div className="text-sm mb-6 flex flex-col sm:flex-row gap-4 items-end mt-10">
+        <div className="text-[8px] mb-6 flex flex-col sm:flex-row gap-4 items-end mt-10">
           <div className="flex-grow">
-            <label className="block text-xsm font-medium text-gray-700 mb-2">
+            <label className="block text-[8px] font-medium text-gray-700 mb-2">
               Select Teacher
             </label>
             <select
               value={selectedTeacher}
               onChange={handleTeacherChange}
-              className="border p-2 w-full rounded-md shadow-sm"
+              className="border text-[8px] p-2 w-full rounded-md shadow-sm"
             >
-              <option value="">Select a teacher</option>
+              <option value="" className='text-[8px]'>Select a teacher</option>
               {teachers.map((teacher) => (
                 <option key={teacher._id} value={teacher._id}>
                   {teacher.name}
@@ -259,7 +283,7 @@ const ViewAttendance = () => {
           </div>
           {classes.length > 0 && (
             <div className="flex-grow">
-              <label className="block text-sm font-medium text-gray-700 mb-2">
+              <label className="block text-[8px] font-medium text-gray-700 mb-2">
                 Select Class
               </label>
               <select
@@ -267,7 +291,7 @@ const ViewAttendance = () => {
                 onChange={(e) => setSelectedClass(e.target.value)}
                 className=" border p-2 w-full rounded-md shadow-sm"
               >
-                <option value="">Select a class</option>
+                <option value="" className='text-[8px]'>Select a class</option>
                 {classes.map((cls) => (
                   <option key={cls} value={cls}>
                     {cls}
@@ -278,15 +302,15 @@ const ViewAttendance = () => {
           )}
           {sections.length > 0 && (
             <div className="flex-grow">
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-              Select Section
+              <label className="block text-[8px] font-medium text-gray-700 mb-2">
+                Select Section
               </label>
               <select
                 value={selectedSection}
                 onChange={(e) => setSelectedSection(e.target.value)}
                 className="border p-2 w-full rounded-md shadow-sm"
               >
-                <option value="">Select a section</option>
+                <option value="" className='text-[8px]'>Select a section</option>
                 {sections.map((section) => (
                   <option key={section} value={section}>
                     {section}
@@ -297,7 +321,7 @@ const ViewAttendance = () => {
           )}
           {subjects.length > 0 && (
             <div className="flex-grow">
-              <label className="block text-sm font-medium text-gray-700 mb-2">
+              <label className="block text-[8px] font-medium text-gray-700 mb-2">
                 Select Subject
               </label>
               <select
@@ -305,7 +329,7 @@ const ViewAttendance = () => {
                 onChange={(e) => setSelectedSubject(e.target.value)}
                 className="border p-2 w-full rounded-md shadow-sm"
               >
-                <option value="">Select a subject</option>
+                <option value="" className='text-[8px]'>Select a subject</option>
                 {subjects.map((subject) => (
                   <option key={subject} value={subject}>
                     {subject}
@@ -315,7 +339,7 @@ const ViewAttendance = () => {
             </div>
           )}
           <div className="flex-grow">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
+            <label className="block text-[8px] font-medium text-gray-700 mb-2">
               Select Month
             </label>
             <input
@@ -327,8 +351,8 @@ const ViewAttendance = () => {
           </div>
           <button
             onClick={handleFetchAttendance}
-            className="bg-black text-white py-3 px-2 text-xs rounded-md shadow-md transition duration-300 ease-in-out flex items-center"
-            style={{ whiteSpace: "nowrap" }}
+            className="btn btn-sm bg-black text-white  text-xs rounded-md shadow-md transition duration-300 ease-in-out flex items-center"
+            style={{ whiteSpace: 'nowrap' }}
             disabled={
               !selectedTeacher ||
               !selectedClass ||
@@ -336,24 +360,43 @@ const ViewAttendance = () => {
               !selectedSubject
             }
           >
-            View<FiEye className="ml-2" />
+            View
+            <FiEye className="ml-2" />
+          </button>
+          <button
+            onClick={handlePrint}
+            className="btn btn-sm bg-black text-white  text-xs rounded-md shadow-md transition duration-300 ease-in-out flex items-center "
+            style={{ whiteSpace: 'nowrap' }}
+            disabled={!attendanceData.length}
+          >
+            PDF
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 ml-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 2a.75.75 0 01.75.75V6h3a.75.75 0 010 1.5h-3v3.25a.75.75 0 01-1.5 0V7.5H6a.75.75 0 010-1.5h3V2.75A.75.75 0 0110 2zm7.86 7.28a.75.75 0 00-1.06 0L12 14.94V11.5a.75.75 0 00-1.5 0v3.44l-4.78-5.72a.75.75 0 00-1.16.94l6 7.2a.75.75 0 001.16 0l6-7.2a.75.75 0 00.1-.84z"
+                clipRule="evenodd"
+              />
+            </svg>
           </button>
         </div>
 
         {loading ? (
-          <div className="text-center py-10">Loading...</div>
+          <div className="text-center py-10" >Loading...</div>
         ) : notFound ? (
           <div className="text-center py-10">Attendance Data Not Found</div>
         ) : attendanceData.length > 0 ? (
-          <div className="mt-4">
-            <div className="overflow-x-auto">
-              <table className="min-w-full bg-white border">
-                <thead>
-                  {renderDays()}
-                </thead>
-                <tbody>{renderAttendanceRows()}</tbody>
-              </table>
-            </div>
+          <div className="mt-4" >
+            <table className="w-full bg-white border-collapse border">
+              <thead>
+                {renderDays()}
+              </thead>
+              <tbody>{renderAttendanceRows()}</tbody>
+            </table>
           </div>
         ) : (
           <div className="text-center py-10">No Data Found</div>
@@ -364,4 +407,3 @@ const ViewAttendance = () => {
 };
 
 export default ViewAttendance;
-
